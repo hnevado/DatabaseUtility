5954aa14b7a461a976c19fd6f2d211cc464aed18
Hector Nevado <hectornevado46@gmail.com>
2024-05-18 18:06:34 +0200

    Correcciones y creado el fichero index.php para empezar las pruebas

--- DatabaseUtility.php
+++ DatabaseUtility.php
@@ -1,46 +1,43 @@
-<?php 
+<?php
 class DatabaseUtility {
 
     private mysqli $mysqli;
     private int $retryCount = 3;
 
     public function __construct(
-        private string $host = 'localhost',
-        private string $db = 'database',
-        private string $user = 'root',
-        private string $password = 'password',
+        private string $host = '',
+        private string $db = '',
+        private string $dbUser = '',
+        private string $password = '',
     ) 
     {
-        $this->connect($this->host, $this->user, $this->password, $this->db);
+        $this->connect($this->host, $this->dbUser, $this->password, $this->db);
     }
 
 
-    private function connect(string $host, string $user, string $password, string $db) : void
+    private function connect(string $host, string $dbUser, string $password, string $db) : void
     {
-        $this->mysqli = new mysqli($host, $user, $password, $db);
-
+        $this->mysqli = new mysqli($host, $dbUser, $password, $db);
+        
         if ($this->mysqli->connect_error) {
             $this->logError("Falló la conexión a la BD: " . $this->mysqli->connect_error);
             die("Error al conectar a la BD: " . $this->mysqli->connect_error);
         }
     }    
 
-    public function lanzarQuery(string $sql, bool $lastId = false, bool $numRows = false) : bool | array {
+    public function lanzarQuery(string $sql, bool $numRows = false) : bool | array {
         //Devolveremos un true o false si la sql se ejecutó correctamente
-        //Si nos pide lastId y/o numRows, un array
+        //Si nos numRows, un array
 
         $array = [];
 
         for ($i = 0; $i < $this->retryCount; $i++) {
             if ($result = $this->mysqli->query($sql)) {
                 
-                if (!$lastId && !$numRows)
+                if (!$numRows)
                  return true;
-                
+
                 $array[] = true;
-                
-                if ($lastId)
-                 $array[]=$this->mysqli->insert_id;
                 
                 if ($numRows)
                  $array[] = $result->num_rows;
@@ -57,6 +54,17 @@ class DatabaseUtility {
             }
         }
        
+    }
+
+    public function insertarQuery() : bool | array {
+        //función dummy - TODO
+        return true;
+    }
+
+    public function eliminarQuery() : bool | array {
+        //función dummy - TODO
+        return true;
+
     }
 
     private function logError(string $message) : void
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require 'DataBaseUtility.php';
+
+//Datos conexión
+$host = 'localhost';
+$db = 'laravel10_livewire';
+$dbUser = 'root';
+$password = '';
+
+
+//Inicializo conexión
+$gestorBD = new DatabaseUtility($host,$db,$dbUser,$password);
+
+//Querys para testing
+
+$sql_select="SELECT * FROM users";
+
+//Fin Querys para testing
+
+//Llamando a los métodos de la clase DataBaseUtility
+$result = $gestorBD->lanzarQuery($sql_select,false);
+var_dump($result);
+
+
+?>
